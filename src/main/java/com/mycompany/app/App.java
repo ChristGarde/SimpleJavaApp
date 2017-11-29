c3aaefe6fdd97a11aacd591ceec59938dc4924bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-11-29 15:07:19 +0000

    Deploy ChristGarde/SimpleJavaApp to github.com/ChristGarde/SimpleJavaApp.git:gh-pages

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String userName = System.getProperty("user.name");
+        System.out.println(userName);
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	    Date date = new Date();
+	    System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
+    }
+}
