fd25d13151d43a5ff72b0c5ba64de481b3e452e3
ChristGarde <Christopher.garde@mycit.ie>
2017-11-29 13:26:23 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String userName = System.getProperty("user.name");
+        System.out.println(userName);
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	    Date date = new Date();
+	    System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
     }
 }
